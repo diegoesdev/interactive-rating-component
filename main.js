6f231dddf476bc14afdea87767fae90a73a21a55
Diego Perales <dperales1313@gmail.com>
2022-09-05 19:39:10 -0400

    Prueba de interacción con los botónes

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ function onClickThanks() {
 }
 
 // Changing the color of the number buttons
+function changeColor() {
+    //document.querySelector(".card__rate--active").style.color = 'red';
+    let activeColor = document.querySelector('.card__rate--active');
+    
+    activeColor.addEventListener('click', () => activeColor.style.backgroundColor = 'hsl(216, 12%, 54%)');
+}
