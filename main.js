0ac7cbf1ee13d4d2fb0416af55af3159b0797535
Diego Perales <dperales1313@gmail.com>
2022-09-06 21:15:48 -0400

    Se agrega la interacción con los botones cuando están activos cambian de color

--- main.js
+++ main.js
@@ -4,9 +4,19 @@ function onClickThanks() {
 }
 
 // Changing the color of the number buttons
-function changeColor() {
-    //document.querySelector(".card__rate--active").style.color = 'red';
-    let activeColor = document.querySelector('.card__rate--active');
+
+// function changeColor() {
+//     //document.querySelector(".card__rate--active").style.color = 'red';
+//     let activeColor = document.querySelector('.card__rate--active');
     
-    activeColor.addEventListener('click', () => activeColor.style.backgroundColor = 'hsl(216, 12%, 54%)');
-}
+//     activeColor.addEventListener('click', () => activeColor.style.backgroundColor = 'hsl(216, 12%, 54%)');
+// }
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+    })
+})
